36aeebf56bb6fa39bf8c4ac3bf167b62fdc3a600
Tajinder Pal Singh <tsingh@beesolvertechnology.com>
2020-10-27 12:29:35 +0530

    Fixed specs - [CA-12013]

--- spec/kickserv/customer_xml_reader_spec.rb
+++ spec/kickserv/customer_xml_reader_spec.rb
@@ -11,13 +11,16 @@ end
 
 RSpec.describe Kickserv::CustomerXmlReader do
   it 'should parse customers' do
-    expect(@customers.size).to eq(2)
-    expect(@customers[0]['id']).to eq('1614793')
-    expect(@customers[0]['name']).to eq('Adrienne Smith')
-    expect(@customers[0]['first-name']).to eq('Adrienne')
-    expect(@customers[0]['last-name']).to eq('Smith')
-    expect(@customers[0]['email']).to eq('asmith221@gmail.com')
-    expect(@customers[0]['phone']).to eq('(404) 413-3294')
+    aggregate_failures do
+      expect(@customers.size).to eq(2)
+      expect(@customers[0]['id']).to eq('1614793')
+      expect(@customers[0]['name']).to eq('Adrienne Smith')
+      expect(@customers[0]['first-name']).to eq('Adrienne')
+      expect(@customers[0]['last-name']).to eq('Smith')
+      expect(@customers[0]['email']).to eq('asmith221@gmail.com')
+      expect(@customers[0]['phone']).to eq('(404) 413-3294')
+      expect(@customers[0]['email-address']).to eq('asmith221@gmail.com')
+    end
   end
 
   # Total number of keys in customer xml.
@@ -29,7 +32,7 @@ RSpec.describe Kickserv::CustomerXmlReader do
 
   # Total number of keys in customer xml.
   it 'should check customer total number of attributes' do
-    expect(@customers[0].keys.length).to eq(36)
+    expect(@customers[0].keys.length).to eq(37)
   end
 
   # Test case for validate xml fields
@@ -44,7 +47,8 @@ RSpec.describe Kickserv::CustomerXmlReader do
                   terms-ref-full-name sales-rep-ref-full-name
                   balance total-balance customer-type-id
                   send-reminders notification-email-address
-                  notify-via-email send-notifications customer-number]
+                  notify-via-email send-notifications customer-number
+                  email-address]
     expect(@customers[0].keys).to contain_exactly(*key_hash)
   end
 end
